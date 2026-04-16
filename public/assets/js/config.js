05d8120f85f75da45958d194aff3264e1cb04869
Loganathan G P <106108501+logusivam@users.noreply.github.com>
2026-04-16 16:48:27 +0530

    fix: update backend URL for production and local environments

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,7 +1,7 @@
 // Local Backend API Endpoint Mapping
 const Ishttps = window.location.protocol === 'https';
 export const BACKEND_URL = Ishttps ? 
-    "" :
-    "http://localhost:5000";
+    "https://tic-tac-toe-api-bqou.onrender.com" :
+    "http://localhost:3000";
 
 
